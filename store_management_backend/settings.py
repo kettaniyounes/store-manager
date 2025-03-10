58653d709a0ac1e19b2441ca6df068e9db0752c0
kettaniyounes <130372975+kettaniyounes@users.noreply.github.com>
2025-03-10 06:18:23 +0100

    Update settings.py

--- store_management_backend/settings.py
+++ store_management_backend/settings.py
@@ -29,12 +29,12 @@ environ.Env.read_env(BASE_DIR / '.env')
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!k$ttj+b_nunw8jz^0ktkekn3p@-&bmy@c!1bqt%$*+c)r)bm)'
+SECRET_KEY = env('SECTET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 0
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
